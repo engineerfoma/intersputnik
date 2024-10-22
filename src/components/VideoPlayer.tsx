3c8959540fb72516d8d1f15ee093ef0bf8fb6190
Nikolay Fomenko <fomen_94@bk.ru>
2024-10-22 21:36:16 +0300

    fix: rename useRef

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useRefё } from 'react'
+import React, { useEffect, useRef } from 'react'
 import videojs from 'video.js'
 import 'video.js/dist/video-js.css'
 import 'videojs-contrib-hls' // Подключение HLS
